dd49d64f0aef851efcafe960ec9ce86bb706402c
Li Tianyou <litianyou97@gmail.com>
2020-08-05 22:38:03 -0400

    Update Robot Room Cleaner.py

--- Robot Room Cleaner/Robot Room Cleaner.py
+++ Robot Room Cleaner/Robot Room Cleaner.py
@@ -35,17 +35,16 @@ class Solution:
     def DFS(self, curdir, curpos, robot):
         robot.clean()
         self.visited.add(curpos)
-        # robot.turnLeft()
 
         for i in range(curdir, curdir+4):
-            d = i%4
-            # robot.turnRight()
+            d = i%4  # 以当前方向为基准，顺时针旋转一周
             newr, newc = curpos[0]+self.DIR[d][0], curpos[1]+self.DIR[d][1]
             if (newr, newc) not in self.visited:
-                if robot.move():
+                if robot.move(): # 计算新的虚拟坐标，可以move且没有visit过
                     self.DFS(d, (newr, newc),robot)
             robot.turnRight()
 
+        # 这里注意，退回原位之后要回归原本方向，所以掉头180，前进，掉头180
         robot.turnRight()
         robot.turnRight()
         robot.move()
@@ -57,7 +56,7 @@ class Solution:
         :type robot: Robot
         :rtype: None
         """
-        self.visited = set()
+        self.visited = set() # 记录虚拟坐标即可。初始坐标是(0,0)
         self.DIR = {0:(-1,0),1:(0,1),2:(1,0),3:(0,-1)}        
         
         self.DFS(0, (0,0), robot)
