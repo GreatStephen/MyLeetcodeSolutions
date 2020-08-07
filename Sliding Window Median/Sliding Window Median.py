f4f2b82ea0f8339e3ec9de4b31991c3cdf4ce767
GreatStephen <32407317+GreatStephen@users.noreply.github.com>
2020-08-07 02:14:13 -0400

    Create Sliding Window Median.py

---
+++ Sliding Window Median/Sliding Window Median.py
@@ -0,0 +1,40 @@
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        maxh, minh = [], []
+        heapq.heapify(maxh)
+        heapq.heapify(minh)
+        
+        ans = []
+        for i,n in enumerate(nums):
+            if i<k:
+                heapq.heappush(maxh, -1*n)
+                while len(maxh)-len(minh)>=2:
+                    heapq.heappush(minh, -1*heapq.heappop(maxh))
+                if i==k-1:
+                    median = -1*maxh[0] if k&1 else (minh[0]-maxh[0])/2
+                    ans.append(median)
+            
+            else:
+                left = i-k
+                if nums[left]<=ans[-1]: # remove element from maxh
+                    idx=0
+                    while idx<len(maxh) and maxh[idx]!=(-1*nums[left]):
+                        idx += 1
+                    maxh[idx] = maxh[-1]
+                    maxh.pop()
+                    heapq.heapify(maxh)                                      
+                else: # remove element from minh
+                    idx = 0
+                    while idx<len(minh) and minh[idx]!=nums[left]:
+                        idx += 1
+                    minh[idx] = minh[-1]
+                    minh.pop()
+                    heapq.heapify(minh)
+                
+                heapq.heappush(maxh, -1*n)
+                while len(maxh)-len(minh)>=2:
+                    heapq.heappush(minh, -1*heapq.heappop(maxh)) 
+                median = -1*maxh[0] if k&1 else (minh[0]-maxh[0])/2
+                ans.append(median)
+        return ans
+                    
