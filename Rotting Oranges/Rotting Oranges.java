c5c1282c2b0cb9331afc64b31f3b23084677c489
Li Tianyou <litianyou97@gmail.com>
2020-08-09 20:24:46 -0400

    Update Rotting Oranges.java

--- Rotting Oranges/Rotting Oranges.java
+++ Rotting Oranges/Rotting Oranges.java
@@ -1,13 +1,16 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        ArrayList<ArrayList<Integer>> queue = new ArrayList<ArrayList>>();
+        int level=0;
+        ArrayList<ArrayList<Integer>> queue = new ArrayList<ArrayList<Integer>>();
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[].length; j++){
-                ArrayList<Integer> rotten = new ArrayList<>(){
-                    add(i);
-                    add(j);
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]==2){
+                    ArrayList<Integer> list = new ArrayList<Integer>();
+                    list.add(i);
+                    list.add(j);
+                    list.add(0);
+                    queue.add(list);
                 }
-                queue.add(rotten);
             }
         }
         while(queue.size()!=0){
@@ -15,17 +18,60 @@ class Solution {
             queue.remove(0);
             int row = rotten.get(0);
             int col = rotten.get(1);
+            int temp_level = rotten.get(2);
+            level=Math.max(level, temp_level);
             if(row-1>=0 && grid[row-1][col]==1){
                 grid[row-1][col]=2;
-                ArrayList<Integer> new_rotten = new ArrayList<>(){
-                    add(row-1);
-                    add(col);
-                }
-                queue.add(new_rotten);
+                ArrayList<Integer> temp_node = new ArrayList<>(){
+                    {
+                        add(row-1);
+                        add(col);
+                        add(temp_level+1);
+                    }
+                    
+                };
+                queue.add(temp_node);
             } 
-            if(row+1<grid[].length && grid[row+1][col]==1) grid[row+1][col]=2;
-            if(col-1>=0 && grid[row][col-1]==1) grid[row][col-1]=2;
-            if(col+1<grid[].length && grid[row][col+1]==1) grid[row][col+1]=2;
+            if(row+1<grid.length && grid[row+1][col]==1){
+                grid[row+1][col]=2;
+                queue.add(new ArrayList<>(){
+                    {
+                        add(row+1);
+                        add(col);
+                        add(temp_level+1);
+                    }
+                    
+                });
+            } 
+            if(col-1>=0 && grid[row][col-1]==1){
+                grid[row][col-1]=2;
+                queue.add(new ArrayList<>(){
+                    {
+                        add(row);
+                        add(col-1);
+                        add(temp_level+1);
+                    }
+                    
+                });
+            } 
+            if(col+1<grid[0].length && grid[row][col+1]==1){
+                grid[row][col+1]=2;
+                queue.add(new ArrayList<>(){
+                    {
+                        add(row);
+                        add(col+1); 
+                        add(temp_level+1);
+                    }
+                    
+                });
+            } 
         }
+        
+        for(int[] i:grid){
+            for(int j:i){
+                if(j==1) return -1;
+            }
+        }
+        return level;
     }
 }
