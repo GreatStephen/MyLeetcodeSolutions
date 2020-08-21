122e151fbf6a564e2b3092b4137f12af4f5100b8
Li Tianyou <litianyou97@gmail.com>
2020-08-20 22:34:53 -0400

    Update Rectangle Area II.py

--- Rectangle Area II/Rectangle Area II.py
+++ Rectangle Area II/Rectangle Area II.py
@@ -1,43 +1,38 @@
 class Solution(object):
-    def rectangleArea(self, rectangles):
-        ans, MOD = 0, 10**9+7
+    def hitBricks(self, grid, hits):
+        # 倒序加DFS。先把所有hits都减1，然后从第一行的元素检查有多少个non-dropping孤岛
+        # 然后倒序添加hits，每添加一个，检查它自己是否变成non-dropping，如果是的话，从它开始DFS，
+        # 把所有和它接触的孤岛全部变成non-dropping。记录这个岛的size。
+        # key：检查倒序添加时，每次添加会增加多少个non-dropping元素
+        def DFS(r,c):
+            if grid[r][c]!=1:
+                return 0
+            ans = 1
+            grid[r][c] = 2
+            for newr, newc in [[r+1,c], [r-1,c], [r,c+1], [r,c-1]]:
+                if 0<=newr<R and 0<=newc<C:
+                    ans += DFS(newr, newc)
+            return ans
         
-        # 这题的思路是逐行扫描，看cur_y这一行有多少个x是标记为正的
-        pq, x = [], []
-        heapq.heapify(pq)
-        for r in rectangles:
-            x.append(r[0])
-            x.append(r[2])
-            heapq.heappush(pq, (r[1], r[0], r[2], 1)) # 1说明这是矩形的底边
-            heapq.heappush(pq, (r[3], r[0], r[2], -1)) # -1说明这是矩形的顶边
+        R,C = len(grid), len(grid[0])
+        for r,c in hits:
+            grid[r][c] -= 1
+        for c in xrange(C):
+            DFS(0, c)
         
-        # 这里是一个节省空间的方法。把所有x的count记录下来，而不是把整个横坐标扒下来
-        # 同时要记录每个x对应的下标，下标之差就是矩形的宽度
-        x.sort()
-        x_i = {v:i for i,v in enumerate(x)}
-        count = [0]*len(x)
         
-        last_y = -1
-        while pq:
-            cur_y, x1, x2, flag = heapq.heappop(pq)
-            if last_y>=0 and cur_y>last_y:
-                l = r = -1
-                for i in xrange(len(x)):
-                    if count[i]>0:
-                        if l<0: l = i
-                        r = i
-                    elif l>=0: # 遇到count=0说明一个底边结束了
-                        ans += (x[i]-x[l])*(cur_y-last_y)
-                        ans %= MOD
-                        l = -1
-                if l>=0:
-                    ans += (x[r]-x[l])*(cur_y-last_y)
-                    ans %= MOD
-                            
-            # 这里很巧妙，不把矩形宽度[i:j]的j也加1，不加1相当于标记了宽度的结束位置
-            # 如果把j也置1，会导致两个底边并排时被当做一个整体，无法分开
-            for i in xrange(x_i[x1], x_i[x2]):
-                count[i] += flag
-            last_y = cur_y
+        ans = []
+        for r,c in hits[::-1]:
+            if grid[r][c]<0:
+                grid[r][c] = 0
+                ans.insert(0,0)
+                continue
+            grid[r][c] = 1
+            res = 0
+            for newr,newc in [[r+1,c], [r-1,c], [r,c+1], [r,c-1]]:
+                if 0<=newr<R and 0<=newc<C and grid[newr][newc]==2 or r==0:
+                    res = DFS(r,c)-1
+                    break
+            ans.insert(0,res)
         
         return ans
